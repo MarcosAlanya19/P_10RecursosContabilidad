b964bd186988e9cf2551fb426d1e68af31ce61df
MarcosAlanya19 <103856433+MarcosAlanya19@users.noreply.github.com>
2022-07-20 02:50:21 -0500

    agregado uno por uno

---
+++ src/dataTable.ts
@@ -0,0 +1,18 @@
+export interface infoTable {
+    id: number;
+    name: string;
+    aporteObligatorio: number;
+    primaSeguro: number;
+    comision: number;
+}
+
+export const infoTable: infoTable[] = [
+    {
+        id:3,
+        name: "3",
+        aporteObligatorio: 3,
+        primaSeguro: 3,
+        comision: 3,
+    },
+
+];
--- src/main.ts
+++ src/main.ts
@@ -29,37 +29,57 @@ const sueldoNeto = (sueldo: number, aporte: number, prima: number, comision: num
 // Aparcion y desaparicion de tablas
 formPlanillaBasica.addEventListener('submit', e => {
   e.preventDefault()
-  const idAporteComision = selectAfp.value;
-  const findAporteComision = aportesComisiones.find(a => a.id === parseInt(idAporteComision))
-  if (!findAporteComision) throw new Error("No se encontro el aporte Comision")
-
-  // Datos de tabla
-  const ingresoBruto = parseInt(sueldo.value);
-  const sueldoBruto= document.querySelector<HTMLTableElement>('#sueldo-bruto')!;
-  sueldoBruto.innerText=`${ingresoBruto.toFixed(2)}`;
-
-  const nameAfp= document.querySelector<HTMLTableElement>('#select-afp')!;
-    nameAfp.innerText=`${findAporteComision.name}`;
-
-  const resultAporte=aporteObligatorio(ingresoBruto,findAporteComision.aporteObligatorio)
-  const aporteAfp= document.querySelector<HTMLTableElement>('#aporte-obligatorio')!;
-  aporteAfp.innerText=`${resultAporte.toFixed(2)}`;
-
-  const resultSeguro=primaSeguro(ingresoBruto,findAporteComision.primaSeguro)
-  const seguroAfp= document.querySelector<HTMLTableElement>('#prima-seguro')!;
-    seguroAfp.innerText=`${resultSeguro.toFixed(2)}`;
-
-  const resultComision=comisionAFP(ingresoBruto,findAporteComision.comision)
-  const comisionAfp= document.querySelector<HTMLTableElement>('#comision-afp')!;
-    comisionAfp.innerText=`${resultComision.toFixed(2)}`;
-    
-
-  const resultNeto=sueldoNeto(ingresoBruto,resultAporte,resultSeguro,resultComision)
-  const netoPlanilla= document.querySelector<HTMLTableElement>('#sueldo-neto')!;
-    netoPlanilla.innerText=`${resultNeto.toFixed(2)}`;
   
+  // info de la tabla
+  infotable();
   // showAndHideTable(tableDiv);
 })
 
+function infotable(){
+  if (!selectAfp) throw new Error("No se encontro el afpInput")
+  const idAporteComision = selectAfp.value;
+  const findAporteComision = aportesComisiones.find(a => a.id === parseInt(idAporteComision))
+  if (!findAporteComision) throw new Error("No se encontro el aporte Comision");
+  // Datos de tabla
 
+  if (!sueldo) throw new Error("No se encontro el inputEmail1")
+  const ingresoBruto = parseInt(sueldo.value);
+  // const sueldoBruto= document.querySelector<HTMLTableElement>('#sueldo-bruto')!;
+  
+
+  // const nameAfp= document.querySelector<HTMLTableElement>('#select-afp')!;
+    // nameAfp.innerText=`${findAporteComision.name}`;
+
+  const resultAporte=aporteObligatorio(ingresoBruto,findAporteComision.aporteObligatorio).toFixed(2);
+  // const aporteAfp= document.querySelector<HTMLTableElement>('#aporte-obligatorio')!;
+  // aporteAfp.innerText=`${resultAporte.toFixed(2)}`;
+
+  const resultSeguro=primaSeguro(ingresoBruto,findAporteComision.primaSeguro).toFixed(2);
+  // const seguroAfp= document.querySelector<HTMLTableElement>('#prima-seguro')!;
+    // seguroAfp.innerText=`${resultSeguro.toFixed(2)}`;
+
+  const resultComision=comisionAFP(ingresoBruto,findAporteComision.comision).toFixed(2);
+  // const comisionAfp= document.querySelector<HTMLTableElement>('#comision-afp')!;
+    // comisionAfp.innerText=`${resultComision.toFixed(2)}`;
+    
+
+  const resultNeto=sueldoNeto(ingresoBruto,resultAporte,resultSeguro,resultComision).toFixed(2);
+  // const netoPlanilla= document.querySelector<HTMLTableElement>('#sueldo-neto')!;
+    // netoPlanilla.innerText=`${resultNeto.toFixed(2)}`;
+
+  // Generar HTML
+    const row = document.createElement('tbody');
+        row.innerHTML = `
+        <tr>
+            <td id="sueldo-bruto">${ingresoBruto}</td>
+            <td id="select-afp">${findAporteComision.name}</td>
+            <td id="aporte-obligatorio">${resultAporte}</td>
+            <td id="prima-seguro">${resultSeguro}</td>
+            <td id="comision-afp">${resultComision}</td>
+            <td id="sueldo-neto">${resultNeto}</td>
+        </tr>
+        `;
+        // Se agrega al tbody
+  tablePlanilla?.appendChild(row);
+}
 
